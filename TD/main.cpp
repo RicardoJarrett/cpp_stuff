5f3152369189dc85a91ea34fad1243e615118554
RicardoJarrett <122801467+RicardoJarrett@users.noreply.github.com>
2023-04-03 02:40:35 +0100

    Stats + UI Update
    Implemented Stats + related UI features

--- TD/main.cpp
+++ TD/main.cpp
@@ -1,6 +1,7 @@
 #define SDL_MAIN_HANDLED
 #include "SDL.h"
 #include "SDL_image.h"
+#include "SDL_ttf.h"
 #include <iostream>
 #include <Windows.h>
 #include <stdlib.h>
@@ -40,12 +41,40 @@ SDL_Texture* loadTexture(std::string path, SDL_Renderer* renderer){
     return retTex;
 }
 
+SDL_Texture* renderText(std::string _text, SDL_Color _col, SDL_Renderer* renderer, TTF_Font* _font)
+{
+    SDL_Texture* retTx = nullptr;
+    SDL_Surface* textSurf = TTF_RenderText_Blended(_font, _text.c_str(), _col);
+    if(textSurf== NULL){
+        std::cout << "Could not render text: " << _text << "\n";
+    }else{
+        retTx = SDL_CreateTextureFromSurface(renderer, textSurf);
+        if(retTx == NULL){
+            std::cout << "Could not create texture from surface\n";
+        }
+        SDL_FreeSurface(textSurf);
+    }
+    return retTx;
+}
+
+class Game;
+struct Stat;
+
 class Button{
+    enum type{
+        func,
+        memberfunc,
+    };
     SDL_Rect area;
     void (*clickFuncPtr)();
+    void (Game::*clickMFuncPtr)(Stat& _stat);
     double bounce;
+    type T;
+    Game* gamePtr;
+    Stat* upgStat;
 public:
     Button(SDL_Rect _area, void (*F)());
+    Button(SDL_Rect _area, void (Game::*F)(Stat& _stat), Game* _game, Stat* _stat);
     bool checkClick(int x, int y);
     bool checkOver(int x, int y);
     void update(double dTime);
@@ -53,11 +82,26 @@ public:
 
 Button::Button(SDL_Rect _area, void (*F)()) : area(_area), clickFuncPtr(F){
     bounce = 0.0;
+    T = func;
+    gamePtr = nullptr;
+    upgStat = nullptr;
+}
+
+Button::Button(SDL_Rect _area, void (Game::*F)(Stat& _stat), Game* _game, Stat* _stat) : area(_area), clickMFuncPtr(F), gamePtr(_game), upgStat(_stat){
+    bounce = 0.0;
+    T = memberfunc;
 }
 
 bool Button::checkClick(int x, int y){
     if((bounce == 0.0) && (checkOver(x, y))){
-        clickFuncPtr();
+        switch(T){
+            case func:
+                clickFuncPtr();
+                break;
+            case memberfunc:
+                (gamePtr->*clickMFuncPtr)(*upgStat);
+                break;
+        }
         bounce = 200.0;
         return true;
     }
@@ -79,9 +123,11 @@ void Button::update(double dTime){
     }
 }
 
+
+
 class Enemy{
 public:
-    double x, y, w, h;
+    double x, y, w, h, spd;
     int hp, maxHp, id;
     static std::queue<int> ids;
     static int getID();
@@ -125,9 +171,23 @@ bool Enemy::takeDamage(int _dmg){
 }
 
 void Enemy::update(double dTime){
-    if(y < 144.0){
-        y += (8.0 * (dTime / 1000.0));
+    if(y < ((SCR_H / 2.0) - 16)){
+        y += (spd * (dTime / 1000.0));
     }}
+
+struct Stat{
+    int stat;
+    int upgradeCost;
+    int level;
+
+    int texID;
+    SDL_Rect texRect;
+    SDL_Texture* textTex;
+    int btnID;
+    SDL_Rect btnTexRect;
+    int iconID;
+    SDL_Rect iconRect;
+};
 
 enum GState{
     gsInit,
@@ -138,8 +198,11 @@ enum GState{
 };
 
 class Game{
-    GState state;
     SDL_Renderer* renderer;
+    TTF_Font* font;
+
+    GState state;
+    int fontSize;
     double fps;
     double fpsTicker;
     bool tick;
@@ -158,12 +221,23 @@ class Game{
     Enemy* targettedEnemy;
     double turretRange;
 
+
+    Stat Dmg;
+    Stat CoinDrop;
+    Stat SpawnRate;
+
+    int CoinID;
+    int coins;
+    SDL_Texture* CoinsTx;
+
     SDL_Rect logoRect;
     SDL_Rect ngbRect;
     SDL_Rect fenceRect;
     SDL_Rect turretBaseRect;
     SDL_Rect turretCannonRect;
     SDL_Point turretPivotOffset;
+    SDL_Rect CoinRect;
+    SDL_Rect CoinsTxRect;
 
     std::vector<Button*> buttons;
     std::vector<SDL_Texture*> textures;
@@ -177,6 +251,12 @@ class Game{
     double fireTicker;
 
     void targetEnemy();
+
+    void statUpgF(Stat& _stat);
+    void drawUpgBtn(Stat& _stat);
+
+
+    void updateCoins(int _coins);
 public:
     Game();
     ~Game();
@@ -199,9 +279,9 @@ Game::Game(){
     tick = false;;
     logoRect = {50, 30, 228, 122};
     ngbRect = {32, 240, 256, 64};
-    fenceRect = {0, 144, 320, 32};
-    turretBaseRect = {128, 240, 64, 64};
-    turretCannonRect = {148, 220, 24, 64};
+    fenceRect = {0, int(SCR_H / 2.0) - 16, SCR_W, 32};
+    turretBaseRect = {int(SCR_W / 2.0) - 32, SCR_H - 80, 64, 64};
+    turretCannonRect = {turretBaseRect.x + 20, turretBaseRect.y - 20, 24, 64};
     turretAngle = 0.0;
     turretPivotOffset = {12, 52};
     targettedEnemy = nullptr;
@@ -216,6 +296,45 @@ Game::Game(){
     spawnDelay = 1000.0;
     spawnTicker = 0.0;
     turretRange = 220.0;
+
+    int statOffset = int(SCR_H / 2.0) + 64;
+
+    Dmg.btnID = -1;
+    Dmg.btnTexRect = {10, statOffset, 64, 16};
+    Dmg.texRect = {Dmg.btnTexRect.x + 16, Dmg.btnTexRect.y + 2, 60, 12};
+    Dmg.upgradeCost = 10;
+    Dmg.textTex = nullptr;
+    Dmg.iconID = -1;
+    Dmg.iconRect = {Dmg.btnTexRect.x + 2, Dmg.btnTexRect.y + 2, 12, 12};
+    Dmg.level = 1;
+    Dmg.stat = 1;
+
+    CoinDrop.btnID = -1;
+    CoinDrop.btnTexRect = {10, statOffset + 20, 64, 16};
+    CoinDrop.texRect = {CoinDrop.btnTexRect.x + 16, CoinDrop.btnTexRect.y + 2, 60, 12};
+    CoinDrop.upgradeCost = 10;
+    CoinDrop.textTex = nullptr;
+    CoinDrop.iconID = -1;
+    CoinDrop.iconRect = {CoinDrop.btnTexRect.x + 2, CoinDrop.btnTexRect.y + 2, 12, 12};
+    CoinDrop.level = 1;
+    CoinDrop.stat = 1;
+
+    SpawnRate.btnID = -1;
+    SpawnRate.btnTexRect = {10, statOffset + 40, 64, 16};
+    SpawnRate.texRect = {SpawnRate.btnTexRect.x + 16, SpawnRate.btnTexRect.y + 2, 60, 12};
+    SpawnRate.upgradeCost = 10;
+    SpawnRate.textTex = nullptr;
+    SpawnRate.iconID = -1;
+    SpawnRate.iconRect = {SpawnRate.btnTexRect.x + 2, SpawnRate.btnTexRect.y + 2, 12, 12};
+    SpawnRate.level = 1;
+    SpawnRate.stat = 1;
+
+    fontSize = 8;
+    renderer = nullptr;
+    font = nullptr;
+    CoinsTx = nullptr;
+    CoinRect = {int(SCR_W / 8.0), 10, 32, 32};
+    CoinsTxRect = {int(SCR_W / 8.0) + 32, 12, 64, 32};
 }
 
 void Game::destroyButtons(){
@@ -228,7 +347,7 @@ void Game::destroyButtons(){
 void Game::destroyTextures(){
     for(std::vector<SDL_Texture*>::iterator it = textures.begin(); it != textures.end(); it++){
         SDL_Texture* T = (*it);
-        delete T;
+        SDL_DestroyTexture(T);
     }
     textures.clear();
     texIDs.clear();
@@ -247,7 +366,6 @@ void Game::destroyEnemies(){
 
 void Game::targetEnemy(){
     double lowestDist = turretRange;
-    double newAngle = 0.0;
     for(std::vector<Enemy*>::iterator it = enemies.begin(); it != enemies.end(); it++){
         Enemy& E = (*(*it));
         double xd = (E.x + (E.w / 2.0)) - (turretBaseRect.x + (turretBaseRect.w / 2.0));
@@ -265,12 +383,13 @@ int Game::spawnNewEnemy(){
     if(int(enemies.size()) < MAX_ENEMIES){
         Enemy* E = new Enemy();
         E->id = Enemy::getID();
-        E->maxHp = 20;
-        E->hp = 20;
-        E->x = std::rand() % (320 - 16);
+        E->maxHp = 5;
+        E->hp = 5;
+        E->x = std::rand() % (SCR_W - 16);
         E->y = -32.0;
         E->w = 16.0;
         E->h = 16.0;
+        E->spd = 32.0;
         enemies.push_back(E);
         return E->id;
     }
@@ -284,6 +403,11 @@ Game::~Game(){
 }
 
 Game game;
+
+void Game::updateCoins(int _coins){
+    coins += _coins;
+    CoinsTx = renderText(std::to_string(coins), {0,0,0}, renderer, font);
+}
 
 void newGameBtn(){
     std::cout << "New Game\n";
@@ -310,7 +434,7 @@ int Game::newTexture(std::string _path){
 }
 
 void Game::drawTexture(int _id, SDL_Rect _dest){
-    if(_id < textures.size()){
+    if(_id < int(textures.size())){
         SDL_RenderCopy(renderer, textures[_id], NULL, &_dest);
     }
 }
@@ -350,11 +474,32 @@ void DrawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_
 }
 
 void Game::drawTurret(int _id, SDL_Rect _dest, double angle){
-    if(_id < textures.size()){
+    if(_id < int(textures.size())){
         SDL_RenderCopyEx(renderer, textures[_id], NULL, &_dest, angle, &turretPivotOffset, SDL_FLIP_NONE);
     }
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     DrawCircle(renderer, turretBaseRect.x  + (turretBaseRect.w / 2.0), turretBaseRect.y + (turretBaseRect.h / 2.0), turretRange);
+}
+
+void Game::statUpgF(Stat& _stat){
+    if(coins >= _stat.upgradeCost){
+        updateCoins(-_stat.upgradeCost);
+        _stat.upgradeCost *= 1.1;
+        SDL_DestroyTexture(_stat.textTex);
+        _stat.level++;
+        _stat.stat++;
+        _stat.textTex = renderText(std::to_string(_stat.level) + ": " + std::to_string(_stat.upgradeCost), {0,0,0}, renderer, font);
+    }
+}
+
+void Game::drawUpgBtn(Stat& _stat){
+    drawTexture(_stat.btnID, _stat.btnTexRect);
+    drawTexture(_stat.iconID, _stat.iconRect);
+    int txw, txh;
+    SDL_QueryTexture(_stat.textTex, NULL, NULL, &txw, &txh);
+    _stat.texRect.w = txw * 1.5;
+    _stat.texRect.h = txh * 1.5;
+    SDL_RenderCopy(renderer, _stat.textTex, NULL, &_stat.texRect);
 }
 
 bool Game::update(double dTime){
@@ -418,6 +563,7 @@ bool Game::update(double dTime){
     }// poll &e
 
     if(state == gsInit){
+        font = TTF_OpenFont("slkscr.ttf", fontSize);
         logoID = newTexture("TowerDefence.png");
         ngbID = newTexture("NewGame.png");
         buttons.push_back(new Button(ngbRect, newGameBtn));
@@ -433,6 +579,25 @@ bool Game::update(double dTime){
         Enemy::initEnemies(MAX_ENEMIES);
         turretBaseID = newTexture("td_basic_towers\\Tower.png");
         turretCannonID = newTexture("td_basic_towers\\Cannon.png");
+
+        CoinID = newTexture("coin.png");
+        updateCoins(0);
+
+        Dmg.btnID = newTexture("button_turq.png");
+        buttons.push_back(new Button(Dmg.btnTexRect, &statUpgF, this, &Dmg));
+        Dmg.textTex = renderText(std::to_string(Dmg.level) + ": " + std::to_string(Dmg.upgradeCost), {0,0,0}, renderer, font);
+        Dmg.iconID = newTexture("bullet.png");
+
+        CoinDrop.btnID = Dmg.btnID;
+        buttons.push_back(new Button(CoinDrop.btnTexRect, &statUpgF, this, &CoinDrop));
+        CoinDrop.textTex = renderText(std::to_string(CoinDrop.level) + ": " + std::to_string(CoinDrop.upgradeCost), {0,0,0}, renderer, font);
+        CoinDrop.iconID = CoinID;
+
+        SpawnRate.btnID = Dmg.btnID;
+        buttons.push_back(new Button(SpawnRate.btnTexRect, &statUpgF, this, &SpawnRate));
+        SpawnRate.textTex = renderText(std::to_string(SpawnRate.level) + ": " + std::to_string(SpawnRate.upgradeCost), {0,0,0}, renderer, font);
+        SpawnRate.iconID = newTexture("Hourglass.png");
+
         updateState(gsGame);
     }else if(state == gsGame){
         drawTexture(fenceID, fenceRect);
@@ -442,13 +607,16 @@ bool Game::update(double dTime){
             spawnTicker -= dTime;
         }else{
             int newID = spawnNewEnemy();
-            spawnTicker = spawnDelay;
+            spawnTicker = spawnDelay / SpawnRate.stat;
         }
         if(lClick){
             if(targettedEnemy != nullptr){
-                targettedEnemy->takeDamage(10);
+                targettedEnemy->takeDamage(Dmg.stat);
             }else{
                 targetEnemy();
+                if(targettedEnemy != nullptr){
+                    targettedEnemy->takeDamage(Dmg.stat);
+                }
             }
         }
         for(std::vector<Enemy*>::iterator it = enemies.begin(); it != enemies.end();){
@@ -459,12 +627,13 @@ bool Game::update(double dTime){
                 Enemy::clearID((*it)->id);
                 delete (*it);
                 it = enemies.erase(it);
+                updateCoins(CoinDrop.stat);
             }else{
                 (*it)->update(dTime);
                 if((*it) == targettedEnemy){
                     DrawCircle(renderer, (*it)->x + ((*it)->w / 2.0), (*it)->y + ((*it)->h / 2.0), (*it)->w);
                 }
-                SDL_Rect enemyRect = {(*it)->x, (*it)->y, (*it)->w, (*it)->h};
+                SDL_Rect enemyRect = {int((*it)->x), int((*it)->y), int((*it)->w), int((*it)->h)};
                 drawTexture(redEnemyID, enemyRect);
                 SDL_Rect hpRect = enemyRect;
                 hpRect.y -= 8;
@@ -474,6 +643,18 @@ bool Game::update(double dTime){
                 it++;
             }
         }
+
+        drawUpgBtn(Dmg);
+        drawUpgBtn(CoinDrop);
+        drawUpgBtn(SpawnRate);
+
+        int txw, txh;
+        drawTexture(CoinID, CoinRect);
+        SDL_QueryTexture(CoinsTx, NULL, NULL, &txw, &txh);
+        CoinsTxRect.w = txw * 3;
+        CoinsTxRect.h = txh * 3;
+        SDL_RenderCopy(renderer, CoinsTx, NULL, &CoinsTxRect);
+
     }
 
     for(std::vector<Button*>::iterator it = buttons.begin(); it != buttons.end(); it++){
@@ -507,12 +688,15 @@ int main(int argc, char* argv[]){
 
     bool initSuccess = true;
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        //error
+        std::cout << "Error - Init Video\n";
+        initSuccess = false;
+    }else if(TTF_Init() == -1){
+        std::cout << "Error - Init TTF\n";
         initSuccess = false;
     }else{
         window = SDL_CreateWindow("TD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCR_W, SCR_H, SDL_WINDOW_SHOWN);
         if(window == NULL){
-            //error
+            std::cout << "Error - Create window\n";
             initSuccess = false;
         }else{
             renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
